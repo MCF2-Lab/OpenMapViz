6de123ad761198c25617f595901d2967b9be344f
suzettem16 <suzzym31@gmail.com>
2025-11-04 11:53:42 -0800

    File Input Read, Received, and Displayed

--- data.js
+++ data.js
@@ -1,16 +1,21 @@
+//reference to the document elements
+const fileInput = document.getElementById("fileInput");
+const fileContent = document.getElementById("fileContent");
+const messageDisplay = document.getElementById("message");
 
-const fileInput = document.getElementById('fileInput');
-const fileContent = document.getElementById('fileContent');
-const messageDisplay = document.getElementById('message');
+//runs handleFile function when user selects a file
+fileInput.addEventListener("change", handleFile); 
 
-
-const file = fileInput.files[0];
-//reading the file
-const read = new FileReader();
-read.onload = function(e) {
-    const content = e.target.result;
-    messageDisplay.textContent = "File received successfully";
-    fileContent.textContent = content;   
+function handleFile(event) {
+    const file = fileInput.files[0];
+    //reading the file
+    const read = new FileReader();
+    read.onload = function(e) {
+        const content = e.target.result;
+        fileContent.textContent = content;  
+        messageDisplay.textContent = "File received successfully";
+        
 }
-
+read.readAsText(file);
+}
 
